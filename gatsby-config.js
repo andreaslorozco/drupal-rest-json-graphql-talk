ac547c3f263c3628ea2a8b348aa6d0c1ea43c50a
Edgar Barrantes <e.barrantes.brais@accenture.com>
2019-07-07 22:30:37 -0600

    Adds gatsby preconnect

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-preconnect",
+      options: {
+        domains: ["http://dev-dc2019-rest-jsonapi-graphql.pantheonsite.io"],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
